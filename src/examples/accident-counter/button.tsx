76d0680966ff074fe42f649d931428a717468e82
Chris Howarth <cthowarth@live.com>
2026-01-30 14:47:47 +0000

    feat: update Button component to handle click events and enhance Counter functionality refactor: remove husky pre-commit hook from package.json refactor: improve NameTag component typing refactor: update Box component to use PropsWithChildren for better type safety

--- src/examples/accident-counter/button.tsx
+++ src/examples/accident-counter/button.tsx
@@ -1,6 +1,6 @@
-export const Button = ({ children }: { children: string }) => {
+export const Button = ({ onClick, children }: { onClick: (e: React.MouseEvent) => void; children: string }) => {
   return (
-    <button className="bg-primary-400 hover:bg-primary-500 rounded px-4 py-2 font-bold text-white">
+    <button onClick={onClick} className="bg-primary-400 hover:bg-primary-500 rounded px-4 py-2 font-bold text-white">
       {children}
     </button>
   );
--- src/examples/accident-counter/counter.tsx
+++ src/examples/accident-counter/counter.tsx
@@ -1,23 +1,27 @@
 import { Card } from '$/common/components/card';
+import { useState } from 'react';
 import { Button } from './button';
 
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <Button>➖ Decrement</Button>
-        <Button>🔁 Reset</Button>
-        <Button>➕ Increment</Button>
+        <Button onClick={() => setCount(count - 1)}>➖ Decrement</Button>
+        <Button onClick={() => setCount(0)}>🔁 Reset</Button>
+        <Button onClick={() => setCount(count + 1)}>➕ Increment</Button>
       </div>
       <form className="flex items-center gap-2" onSubmit={(e) => e.preventDefault()}>
         <input
           className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
           type="number"
-          value={0}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.valueAsNumber)}
         />
-        <Button>Update Counter</Button>
+        <Button onClick={() => setCount(inputValue)}>Update Counter</Button>
       </form>
     </Card>
   );
